47d862573cc9f76088421cf98ab62071c996c3a3
Dan Adiletta <dan.adiletta@gmail.com>
2015-05-17 16:07:33 -0400

    Adding schema fixes to price decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510194311) do
+ActiveRecord::Schema.define(version: 20150512123847) do
 
   create_table "orders", force: true do |t|
     t.integer "user_id"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20150510194311) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "price"
+    t.decimal  "price"
   end
 
 end

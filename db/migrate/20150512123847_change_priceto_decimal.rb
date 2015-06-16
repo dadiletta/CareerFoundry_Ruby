6f309453d5c35728fcbbc52205baeb2dbf7b178a
dadiletta <dan.adiletta@gmail.com>
2015-06-16 15:46:09 -0400

    Revert "Explicit conversion for price column"
    This reverts commit 068af63564319b1cce9e04f3c0a92becaf2bcd5d.

--- db/migrate/20150512123847_change_priceto_decimal.rb
+++ db/migrate/20150512123847_change_priceto_decimal.rb
@@ -1,5 +1,5 @@
-lass ChangePriceToNumeric < ActiveRecord::Migration
-    def change
-        change_column :parts, :price, 'numeric USING CAST(price AS numeric)'
-    end
+class ChangePricetoDecimal < ActiveRecord::Migration
+  def change
+      change_column :products, :price, :decimal
+  end
 end

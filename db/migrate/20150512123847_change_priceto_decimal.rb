b70f8e1394a0f5bc5aa85ce8e789efb2b1f87337
dadiletta <dan.adiletta@gmail.com>
2015-06-17 20:59:52 -0400

    Joe helps loop to convert string to float

--- db/migrate/20150512123847_change_priceto_decimal.rb
+++ db/migrate/20150512123847_change_priceto_decimal.rb
@@ -1,5 +1,14 @@
 class ChangePricetoDecimal < ActiveRecord::Migration
   def change
+      Product.all.each do |p|
+          p.price = 
+          begin 
+              Float(p.price) 
+          rescue
+              0.0 
+        end
+          p.save
+      end
       change_column :products, :price, :decimal
   end
 end

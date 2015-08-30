e8bccb0c8cb4c09bd41aefeeb081c8ee5aa2d530
dadiletta <dan.adiletta@gmail.com>
2015-08-30 11:23:39 -0400

    Fixed email template and controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,7 @@ class StaticPagesController < ApplicationController
       @name = params[:name]
       @email = params[:email]
       @message = params[:message]
+      UserMailer.contact_form(@email, @name, @message).deliver
       ActionMailer::Base.mail(:from => @email, 
             :to => 'your-email@example.com', 
             :subject => "A new contact form message from #{@name}", 

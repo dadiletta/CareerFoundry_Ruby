7303910975aefd5d396825d9e09f03f2f37e5faa
dadiletta <dan.adiletta@gmail.com>
2015-09-02 20:23:05 -0400

    Failed attempt to improve controller tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+login_user
+
 describe StaticPagesController, :type => :controller do
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do

0808ebc3f5462dee87e9c73b7d0fbb7d4d99fe2b
dadiletta <dan.adiletta@gmail.com>
2015-09-07 18:29:06 -0400

    More mailer fixes attempted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :orders
+    
+    private
+
+      def welcome_email 
+        UserMailer.welcome_message(self).deliver
+      end
+    
 end
